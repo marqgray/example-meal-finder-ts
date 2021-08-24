79c6c0e30eb177038110f53220c59e2387e3e204
Marq Gray <marquis.s.sandifer@gmail.com>
2021-08-24 17:05:48 -0500

    Added meal details

--- script.ts
+++ script.ts
@@ -30,7 +30,44 @@ const app = new (class {
   getMealById(mealId) {
     fetch(`http://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
       .then((res) => res.json())
-      .then((data) => {});
+      .then((data: MealFinderApi) => {
+        const meal = data.meals[0];
+        this.addMealToDom(meal);
+      });
+  }
+
+  addMealToDom(meal: Meal) {
+    const ingredients = [];
+
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`]) {
+        ingredients.push(
+          `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+        );
+      } else {
+        break;
+      }
+    }
+
+    this.htmlElements.singleMealElement.innerHTML = `
+      <div class="single-meal">
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+        <div class="single-meal-info">
+            ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+            ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+        </div>
+        <div class="main">
+            <p>${meal.strInstructions}</p>
+            <h2>Ingredients</h2>
+            <ul>
+              ${ingredients
+                .map((ingredient) => `<li>${ingredient}</li>`)
+                .join("")}
+            </ul>
+        </div>
+      </div>
+    `;
   }
 
   searchMeal(e) {
